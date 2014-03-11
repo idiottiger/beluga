05d5991ca0d8a12fbda4ed671085abe2d86dc076
FrodoChen <idiottiger@gmail.com>
2014-03-11 14:39:41 +0800

    finish the inject service.

--- main/src/com/idiot2ger/beluga/inject/Injector.java
+++ main/src/com/idiot2ger/beluga/inject/Injector.java
@@ -6,7 +6,22 @@ import java.util.HashSet;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.DownloadManager;
+import android.app.KeyguardManager;
+import android.app.NotificationManager;
+import android.app.SearchManager;
+import android.app.UiModeManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.PowerManager;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 
 /**
@@ -34,6 +49,11 @@ public class Injector {
   }
 
   public void inject(Object object) {
+    inject(object, object);
+  }
+
+
+  public void inject(Object object, Object injectProvider) {
     if (object == null) {
       throw new IllegalArgumentException("when inject, the object must not be null");
     }
@@ -52,6 +72,8 @@ public class Injector {
           InjectItem item = null;
           if (field.isAnnotationPresent(InjectView.class)) {
             item = new InjectViewItem(field, field.getAnnotation(InjectView.class));
+          } else if (field.isAnnotationPresent(InjectService.class)) {
+            item = new InjectServiceItem(field);
           }
           if (item != null) {
             set.add(item);
@@ -64,13 +86,13 @@ public class Injector {
 
     if (injectSet != null) {
       for (InjectItem item : injectSet) {
-        item.initField(object, object);
+        item.initField(object, injectProvider);
       }
     }
   }
 
 
-  private abstract class InjectItem {
+  private static abstract class InjectItem {
     protected Field field;
 
     public InjectItem(Field f) {
@@ -80,7 +102,7 @@ public class Injector {
     public abstract void initField(Object caller, Object object);
   }
 
-  private class InjectViewItem extends InjectItem {
+  private static class InjectViewItem extends InjectItem {
 
     private int id;
 
@@ -112,6 +134,51 @@ public class Injector {
         e.printStackTrace();
       }
     }
+  }
+
+  private static class InjectServiceItem extends InjectItem {
+
+    private static HashMap<Class<?>, String> sServiceMap = new HashMap<Class<?>, String>();
+
+    static {
+      sServiceMap.put(WindowManager.class, Context.WINDOW_SERVICE);
+      sServiceMap.put(LayoutInflater.class, Context.LAYOUT_INFLATER_SERVICE);
+      sServiceMap.put(ActivityManager.class, Context.ACTIVITY_SERVICE);
+      sServiceMap.put(PowerManager.class, Context.POWER_SERVICE);
+      sServiceMap.put(AlarmManager.class, Context.ALARM_SERVICE);
+      sServiceMap.put(NotificationManager.class, Context.NOTIFICATION_SERVICE);
+      sServiceMap.put(KeyguardManager.class, Context.KEYGUARD_SERVICE);
+      sServiceMap.put(SearchManager.class, Context.SEARCH_SERVICE);
+      sServiceMap.put(Vibrator.class, Context.VIBRATOR_SERVICE);
+      sServiceMap.put(ConnectivityManager.class, Context.CONNECTIVITY_SERVICE);
+      sServiceMap.put(WifiManager.class, Context.WIFI_SERVICE);
+      sServiceMap.put(ConnectivityManager.class, Context.CONNECTIVITY_SERVICE);
+      sServiceMap.put(InputMethodManager.class, Context.INPUT_METHOD_SERVICE);
+      sServiceMap.put(UiModeManager.class, Context.UI_MODE_SERVICE);
+      sServiceMap.put(DownloadManager.class, Context.DOWNLOAD_SERVICE);
+    }
+
+    public InjectServiceItem(Field f) {
+      super(f);
+    }
+
+    @Override
+    public void initField(Object caller, Object object) {
+      Class<?> serviceType = field.getType();
+      String serviceName = sServiceMap.get(serviceType);
+      if (serviceName != null) {
+        try {
+          field.setAccessible(true);
+          Object value = ((Context) object).getSystemService(serviceName);
+          field.set(caller, field.getType().cast(value));
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
   }
 
 }
