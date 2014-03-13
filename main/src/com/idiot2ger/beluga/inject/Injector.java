23bb7b04ba122d00cad94bd74881cf399e20ea83
FrodoChen <idiottiger@gmail.com>
2014-03-13 09:23:33 +0800

    use inject instead of injectservice.

--- main/src/com/idiot2ger/beluga/inject/Injector.java
+++ main/src/com/idiot2ger/beluga/inject/Injector.java
@@ -39,9 +39,29 @@ public final class Injector {
 
   }
 
-  private final static Map<Class<?>, HashSet<InjectItem>> mInjectItemMap = new HashMap<Class<?>, HashSet<InjectItem>>();
+  private final static Map<Class<?>, HashSet<InjectItem>> sInjectItemMap = new HashMap<Class<?>, HashSet<InjectItem>>();
 
-  private final static Map<Class<?>, Object> mGlobalInjectMap = new HashMap<Class<?>, Object>();
+  private final static Map<Class<?>, Object> sGlobalInjectMap = new HashMap<Class<?>, Object>();
+  private static HashMap<Class<?>, String> sServiceMap = new HashMap<Class<?>, String>();
+
+
+  static {
+    sServiceMap.put(WindowManager.class, Context.WINDOW_SERVICE);
+    sServiceMap.put(LayoutInflater.class, Context.LAYOUT_INFLATER_SERVICE);
+    sServiceMap.put(ActivityManager.class, Context.ACTIVITY_SERVICE);
+    sServiceMap.put(PowerManager.class, Context.POWER_SERVICE);
+    sServiceMap.put(AlarmManager.class, Context.ALARM_SERVICE);
+    sServiceMap.put(NotificationManager.class, Context.NOTIFICATION_SERVICE);
+    sServiceMap.put(KeyguardManager.class, Context.KEYGUARD_SERVICE);
+    sServiceMap.put(SearchManager.class, Context.SEARCH_SERVICE);
+    sServiceMap.put(Vibrator.class, Context.VIBRATOR_SERVICE);
+    sServiceMap.put(ConnectivityManager.class, Context.CONNECTIVITY_SERVICE);
+    sServiceMap.put(WifiManager.class, Context.WIFI_SERVICE);
+    sServiceMap.put(ConnectivityManager.class, Context.CONNECTIVITY_SERVICE);
+    sServiceMap.put(InputMethodManager.class, Context.INPUT_METHOD_SERVICE);
+    sServiceMap.put(UiModeManager.class, Context.UI_MODE_SERVICE);
+    sServiceMap.put(DownloadManager.class, Context.DOWNLOAD_SERVICE);
+  }
 
 
   /**
@@ -52,7 +72,7 @@ public final class Injector {
    * @param object
    */
   public static void putGlobalInject(Class<?> cls, Object object) {
-    mGlobalInjectMap.put(cls, object);
+    sGlobalInjectMap.put(cls, object);
   }
 
   public static void inject(Object object) {
@@ -68,8 +88,8 @@ public final class Injector {
     // search two maps
 
     HashSet<InjectItem> injectSet = null;
-    if (mInjectItemMap.containsKey(cls)) {
-      injectSet = mInjectItemMap.get(cls);
+    if (sInjectItemMap.containsKey(cls)) {
+      injectSet = sInjectItemMap.get(cls);
     } else {
       // query all inject item to the maps
       Field[] fieldList = cls.getDeclaredFields();
@@ -79,16 +99,17 @@ public final class Injector {
           InjectItem item = null;
           if (field.isAnnotationPresent(InjectView.class)) {
             item = new InjectViewItem(field, field.getAnnotation(InjectView.class));
-          } else if (field.isAnnotationPresent(InjectService.class)) {
-            item = new InjectServiceItem(field);
           } else if (field.isAnnotationPresent(Inject.class)) {
             // check the field class type's annotation
             final Class<?> fieldType = field.getType();
             if (fieldType.isAnnotationPresent(Singleton.class)) {
               item = new InjectSingletonItem(field);
             }// find in the global inject
-            else if (mGlobalInjectMap.containsKey(fieldType)) {
-              setField(field, object, mGlobalInjectMap.get(fieldType));
+            else if (sGlobalInjectMap.containsKey(fieldType)) {
+              setField(field, object, sGlobalInjectMap.get(fieldType));
+              continue;
+            } else if (sServiceMap.containsKey(fieldType)) {
+              item = new InjectServiceItem(field);
             }
           }
           if (item != null) {
@@ -97,7 +118,7 @@ public final class Injector {
         }
 
         if (!set.isEmpty()) {
-          mInjectItemMap.put(cls, set);
+          sInjectItemMap.put(cls, set);
           injectSet = set;
         }
       }
@@ -109,7 +130,6 @@ public final class Injector {
       }
     }
   }
-
 
   private static void setField(Field field, Object caller, Object value) {
     try {
@@ -167,26 +187,6 @@ public final class Injector {
   }
 
   private static class InjectServiceItem extends InjectItem {
-
-    private static HashMap<Class<?>, String> sServiceMap = new HashMap<Class<?>, String>();
-
-    static {
-      sServiceMap.put(WindowManager.class, Context.WINDOW_SERVICE);
-      sServiceMap.put(LayoutInflater.class, Context.LAYOUT_INFLATER_SERVICE);
-      sServiceMap.put(ActivityManager.class, Context.ACTIVITY_SERVICE);
-      sServiceMap.put(PowerManager.class, Context.POWER_SERVICE);
-      sServiceMap.put(AlarmManager.class, Context.ALARM_SERVICE);
-      sServiceMap.put(NotificationManager.class, Context.NOTIFICATION_SERVICE);
-      sServiceMap.put(KeyguardManager.class, Context.KEYGUARD_SERVICE);
-      sServiceMap.put(SearchManager.class, Context.SEARCH_SERVICE);
-      sServiceMap.put(Vibrator.class, Context.VIBRATOR_SERVICE);
-      sServiceMap.put(ConnectivityManager.class, Context.CONNECTIVITY_SERVICE);
-      sServiceMap.put(WifiManager.class, Context.WIFI_SERVICE);
-      sServiceMap.put(ConnectivityManager.class, Context.CONNECTIVITY_SERVICE);
-      sServiceMap.put(InputMethodManager.class, Context.INPUT_METHOD_SERVICE);
-      sServiceMap.put(UiModeManager.class, Context.UI_MODE_SERVICE);
-      sServiceMap.put(DownloadManager.class, Context.DOWNLOAD_SERVICE);
-    }
 
     public InjectServiceItem(Field f) {
       super(f);
